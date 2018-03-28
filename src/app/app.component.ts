f6475e0f157de226d678c08fddca6ba89638e6f8
DESKTOP-OKVKOOA\cmina <mina@wannabit.io>
2018-03-28 19:01:35 +0900

    ethereum 지갑주소 생성 및 가져오기

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,121 @@
 import { Component } from '@angular/core';
 import * as EthereumWallet from "../../node_modules/ethereumjs-wallet";
-import * as HDKey from "../../node_modules/ethereumjs-wallet/hdkey";
+import * as Hdkey from "../../node_modules/ethereumjs-wallet/hdkey";
 import TransportU2F from "@ledgerhq/hw-transport-u2f";
 import AppBtc from "@ledgerhq/hw-app-btc";
+import AppEth from "@ledgerhq/hw-app-eth";
+import * as BIP39 from "../../node_modules/bip39";
+import * as Web3 from "../../node_modules/web3";
+import * as contractABI from "../../node_modules/human-standard-token-abi";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
 
   ledgerAddrList = [];
 
+  ETHAddrListByL = [];
+  ETHAddrListByM = [];
+  ETHMnemonics: string;
+
   constructor() {
     this.initWallet();
   }
 
+  createMnemonics() {
+    const mnemonic = BIP39.generateMnemonic(256);
+    this.ETHMnemonics = mnemonic;
+  }
+
+  getETHAddrByM(i) {
+    const path = `m/44'/60'/0'/0/${i}`;
+    const hdwallet = Hdkey.fromMasterSeed(BIP39.mnemonicToSeed(this.ETHMnemonics));
+    const wallet = hdwallet.derivePath(path).getWallet();
+    // const privKey = wallet.getPrivateKey().toString('hex');
+    const address = `0x${wallet.getAddress().toString('hex')}`;
+
+    return address;
+  }
+
+  //5개의 eth주소를 array로
+  getETHAddrListByM(j) {
+    let array = [];
+    for (let i = 5 * j; i < 5 * (j + 1); i++) {
+      const data = this.getETHAddrByM(i);
+      array.push(data);
+    }
+    this.ETHAddrListByM = array;
+    // return  array;
+  };
+
+  async getETHAddrByL(i) {
+    let transport = await TransportU2F.create(5000, true);
+    let eth = new AppEth(transport);
+    
+    const path = "m/44'/60'/0'/" + i;
+    // const path = "m/44'/1'/0'/0/" + i;
+    let addr;
+    await eth.getAddress(path, false, false).then(res => {
+      addr = res;
+      console.log(path, addr);
+    })
+    return addr;
+  }
+
+  //5개의 eth주소를 array로 by ledger
+  async getETHAddrListByL(j) {
+    let array = [];
+    for (let i = 5 * j; i < 5 * (j + 1); i++) {
+      const data = await this.getETHAddrByL(i);
+      console.log("getETHAddrListByL", data);
+      array.push(data);
+    }
+    console.log("getETHAddrListByL", array);
+    this.ETHAddrListByL = array;
+        // return array;
+  };
+
+  getETHAddrByLBtn () {
+    this.getETHAddrListByL(1);
+  }
+
+  getETHAddrByMBtn() {
+    this.getETHAddrListByM(1);
+  }
+
+
+
+  btnLedger(){
+    async function getEtherAddress() {
+      let transport;
+      try {
+        transport = await TransportU2F.create();
+        let eth = new AppEth(transport);
+
+        eth.getAddress("m/44'/1'/0'/0",false,false).then(function(publicKey) {
+          console.log(JSON.stringify(publicKey));
+        }).catch(function(err) {
+          console.log(err);
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    } 
+    getEtherAddress();
+  }
+
   connectLedger() {
     this.getLedgerAddrArray(0);
-
   }
 
   initWallet() {
     let walletjs = EthereumWallet.generate();
     let privKey = walletjs.getPrivateKeyString();
     let pubKey = walletjs.getPublicKeyString();
-    console.log("privKey", privKey, pubKey, HDKey);
+    console.log("privKey", privKey, pubKey);
   }
 
   // 주소 가져오기
@@ -90,5 +179,6 @@ export class AppComponent {
     );
     console.log("result: " + result);
     return result;
-  };
+  }
+
 }
